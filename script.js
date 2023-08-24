cd77acfe99e6e24d60538c2746f1204672f76f62
MohamedYassineAbid <117313298+MohamedYassineAbid@users.noreply.github.com>
2023-08-24 16:14:40 +0100

    new

---
+++ script.js
@@ -0,0 +1,12 @@
+//camel notation in js and java 
+//firstName
+
+let person ={
+    name:"bid",
+
+};
+person.name="hzllo";
+let selectedCOlor=["red","yello"];
+selectedCOlor[2]="dark";
+console.log(selectedCOlor)
+console.log(person.name);
